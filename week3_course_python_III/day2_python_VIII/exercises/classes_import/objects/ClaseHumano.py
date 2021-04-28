b093ca1324d6d9d058cce6ae49c2ac5516e42ad9
maserrca <marina.serranoc@gmail.com>
2021-04-28 17:25:41 +0200

    Añadido día 3 y ejercicios

--- week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseHumano.py
@@ -1,7 +1,7 @@
 import sys, os
 volver = os.path.dirname(__file__)
 sys.path.append(volver)
-class Humano:
+class humano:
     def __init__(self, nombre, armadura, nivel, ataque, ojos=2,piernas=2, dientes=32, salud=100):
         self.ojos = 2
         self.piernas = 2
@@ -9,8 +9,19 @@ class Humano:
         self.nombre = nombre
         self.armadura = armadura
         self.ataque = ataque
+        self.nivel = nivel
         self.salud = 100
     def atacar(self, orco):
-    
-    def no_vivo(self, )
+        orco.salud = orco.salud - (self.ataque - orco.armadura)
+        print(orco.salud)
+        return(orco.salud)
+    def no_vivo(self):
+        if self.salud <= 0:
+            return True
+        else:
+            return False
+    def mostrar(self):
+        print("Nombre", self.nombre, "|", "Armadura:", self.armadura ,"|", "Nivel:", self.nivel ,"|", "Ataque:", self.ataque,"|", "Ojos:", self.ojos,"|", "Piernas:", self.piernas, "|", "Dientes:", self.dientes, "|","Salud:", self.salud)
+
+
         
--- week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
+++ week3_course_python_III/day2_python_VIII/exercises/classes_import/objects/ClaseOrco.py
@@ -1,4 +1,5 @@
-class Orco:
+
+class orco:
     def __init__(self, nombre, armadura, nivel, ataque, ojos=2,piernas=2, dientes=32, salud=100):
         self.ojos = 2
         self.piernas = 2
@@ -8,4 +9,16 @@ class Orco:
         self.ataque = ataque
         self.salud = 100
     def atacar(self, humano):
-        from ClaseHumano import 
+        humano.salud = humano.salud - (self.ataque - humano.armadura)
+        print(humano.salud)
+        return humano.salud
+    def no_vivo(self):
+        if self.salud <= 0:
+            return True
+        else:
+            return False
+    def mostrar(self):
+        print("Nombre", self.nombre, "|", "Armadura:", self.armadura ,"|", "Nivel:", self.nivel ,"|", "Ataque:", self.ataque,"|", "Ojos:", self.ojos,"|", "Piernas:", self.piernas, "|", "Dientes:", self.dientes, "|","Salud:", self.salud)
+
+
+        
