5814d22c5ffdce0841f586dc3e6e20b672f7b26b
maserrca <marina.serranoc@gmail.com>
2021-05-16 23:08:24 +0200

    Actualización y hundir la flota

---
+++ week4_EDA_np_pd_json_apis_regex/Homework/Hundir_la_flota/definitely.py
@@ -0,0 +1,12 @@
+x= randint(0,6)
+y= randint(0,6)
+def func_random(jugador1,jugador2):
+    print(x)
+    if x > y:
+        print(x)
+        return Jugador1
+    else:
+        print(y) 
+        return jugador2
+
+func_random(input("Nombre 1"),input("Nombre 2"))
